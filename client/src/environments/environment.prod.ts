1e4c06258db7e3cf4866ccb7cb4286979060695b
Aayush Naik <aayushnai@gmail.com>
2022-11-12 22:37:32 +0530

    (fix) add env

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -3,4 +3,5 @@ export const environment = {
   PROVIDER_URL:
     'https://compatible-bitter-thunder.matic-testnet.quiknode.pro/55aaf4df295c255581bea7723fe96dc70dc81ab9/',
   MAGIC_API_KEY: 'pk_live_F961216011ACF2B8',
+  IPFS_URL: 'https://ipfs.shiro.network',
 };
