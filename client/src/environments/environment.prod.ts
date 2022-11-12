3727308c19a6b2227b76714c34c505e428f0bf32
Aayush Naik <aayushnai@gmail.com>
2022-11-12 12:01:19 +0530

    (feat) add env

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   PROVIDER_URL:
-  'https://compatible-bitter-thunder.matic-testnet.quiknode.pro/55aaf4df295c255581bea7723fe96dc70dc81ab9/',
-MAGIC_API_KEY: 'pk_live_F961216011ACF2B8',
+    'https://compatible-bitter-thunder.matic-testnet.quiknode.pro/55aaf4df295c255581bea7723fe96dc70dc81ab9/',
+  MAGIC_API_KEY: 'pk_live_F961216011ACF2B8',
 };
