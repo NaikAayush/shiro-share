7f218c2e4f89dce02fb33dea6634d7e9db3b10fd
Aayush Naik <aayushnai@gmail.com>
2022-11-18 22:44:54 +0530

    (feat) add landing page

---
+++ landing/src/pages/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div className="flex flex-col min-h-screen overflow-hidden">
+    <!-- Site header -->
+    <Header />
+
+    <!-- Page content -->
+    <main class="grow">
+      <Hero />
+      <PressLogos />
+      <Providers />
+      <Features />
+      <Pricing />
+      <Resources />
+      <Cta />
+    </main>
+
+    <!-- Site footer -->
+    <div class="pb-10"></div>
+  </div>
+</template>
+
+<script>
+import Header from "../partials/Header.vue";
+import Hero from "../partials/Hero.vue";
+import PressLogos from "../partials/PressLogos.vue";
+import Providers from "../partials/Providers.vue";
+import Features from "../partials/Features.vue";
+import Pricing from "../partials/Pricing.vue";
+import Resources from "../partials/Resources.vue";
+import Cta from "../partials/Cta.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    Hero,
+    PressLogos,
+    Providers,
+    Features,
+    Pricing,
+    Resources,
+    Cta,
+  },
+};
+</script>
---
+++ landing/src/partials/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header class="absolute w-full z-30">
+    <div class="max-w-6xl mx-auto px-4 sm:px-6">
+      <div class="flex items-center justify-between h-16 md:h-20">
+        <!-- Site branding -->
+        <div class="shrink-0 mr-4">
+          <!-- Logo -->
+          <router-link class="block" to="/" aria-label="Cruip">
+            <img class="h-10" src="../images/shiro-white.svg" alt="" />
+          </router-link>
+        </div>
+
+        <!-- Desktop navigation -->
+        <nav class="flex grow">
+          <!-- Desktop sign in links -->
+          <ul class="flex grow justify-end flex-wrap items-center">
+            <li class="ml-3">
+              <router-link
+                class="btn-sm text-white bg-gradient-to-t from-blue-600 to-blue-400 hover:to-blue-500 w-full shadow-lg group"
+                to="#0"
+              >
+                Get Started
+                <span
+                  class="tracking-normal text-blue-200 group-hover:translate-x-0.5 transition-transform duration-150 ease-in-out ml-1"
+                  >-&gt;</span
+                >
+              </router-link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
